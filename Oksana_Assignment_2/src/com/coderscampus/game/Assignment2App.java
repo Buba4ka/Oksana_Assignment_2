12b29b55825d95d719d28bbe957356803a84a6ad
Buba4ka <61472783+Buba4ka@users.noreply.github.com>
2022-03-27 15:07:05 -0500

    v2
    Changes to the code

--- Oksana_Assignment_2/src/com/coderscampus/game/Assignment2App.java
+++ Oksana_Assignment_2/src/com/coderscampus/game/Assignment2App.java
@@ -13,55 +13,53 @@ public class Assignment2App {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Pick a number between 1 and 100");
-		
-		int inputInt = Integer.valueOf(scanner.nextLine());
-		while (inputInt <1 || inputInt >100) {
-			
-			System.out.println("Your guess is not between 1 and 100, please try again");
-			inputInt = Integer.valueOf(scanner.nextLine());
-			
-		}
-		messageToDisplay(inputInt, random,scanner);
 
-		
+		int inputInt = Integer.valueOf(scanner.nextLine());
+
+//		if (inputInt < 1 || inputInt > 100) {
+//
+//			System.out.println("Your guess is not between 1 and 100, please try again");
+//			inputInt = Integer.valueOf(scanner.nextLine());
+//
+//		}
+		messageToDisplay(inputInt, random, scanner);
 
 	}
 
 	public static void messageToDisplay(int playerNumber, int randomNum, Scanner scanner) {
+		int count = 1;
 		
-		int count =1;
 
 		while (count < 5 && playerNumber != randomNum) {
-			if (playerNumber < randomNum) {
+			
+			if (playerNumber > 100 || playerNumber < 1) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+				playerNumber = Integer.valueOf(scanner.nextLine()); 
+				
+			} else if (playerNumber < randomNum) {
 
 				System.out.println("Please pick a higher number");
 				playerNumber = Integer.valueOf(scanner.nextLine());
 				count++;
 
 			} else if (playerNumber > randomNum) {
+
 				System.out.println("Please pick a lower number");
 				playerNumber = Integer.valueOf(scanner.nextLine());
 				count++;
 
-			} else  {
-				
-				System.out.println("You win!");	
-				count++;
-				scanner.close();
 			}
+		} 
+		if (playerNumber == randomNum) {
+			System.out.println("You win!");
 			
-		}
-		
-		if (playerNumber != randomNum && count == 5) {
-			
+		}  else if (count == 5 && playerNumber != randomNum) {
 			System.out.println("You lose, the number to guess was " + randomNum);
-			scanner.close();
-		}
-		
-		
+		} 
+		scanner.close();
 
+		
 	}
 
-
-
+	
 }
